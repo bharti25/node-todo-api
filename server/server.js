a0a3bc0ff99caa2999a60217ab1470ae4cdc5d73
Bharti Bulchandani <bharti@paynpark.xyz>
2018-05-21 17:11:45 +0530

    POST method added

---
+++ server/server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var	bodyParser = require('body-parser');
+
+var {mongoose}=  require('./db/mongoose');
+var	{Todo} = require('./models/todo');
+var	{User} = require('./models/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todo', (request, response) => {
+	var todo = new Todo({
+		text: request.body.text,
+		completed: request.body.completed,
+		completedAt: request.body.completedAt
+	});
+
+	todo.save().then((doc) => {
+		response.send(doc);
+	}, (e) => {
+		response.status(400).send(e);
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Started app on port 3000');
+});
+
+// var objTodo = new Todo({
+// 	text: 'Read'
+// });
+
+// objTodo.save().then((doc) => {
+// 	console.log('The saved todo is', doc);
+// }, (e) => {
+// 	console.log('Unable to save the todo.');
+// });
+
+// var newobjTodo = new Todo({
+// 	// text: 'New Todo',
+// 	// completed: false,
+// 	// completedAt: 140
+// 	text: 'A Todo'
+// });
+
+// newobjTodo.save().then((doc) => {
+// 	console.log('Saved todo', doc);
+// }, (e) => {
+// 	console.log('Unable to save the todo.', e);
+// });
+
+// var objUser = new User({
+// 	email: 'abc@abc.com'
+// });
+
+// objUser.save().then((doc) => {
+// 	console.log('Saved User', doc);
+// }, (e) => {
+// 	console.log('Unable to save the user.',e);
+// });
