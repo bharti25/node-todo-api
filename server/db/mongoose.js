03b3f6012082fd4e0060fa6b2737bfd2f8c758a6
Bharti Bulchandani <bharti@paynpark.xyz>
2018-07-09 11:49:20 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// let db = {
-// 	localhost: 'mongodb://127.0.0.1:27017/Todo',
-// 	mLab: 'mongodb://bharti25:Bh@rtiIsro2511@ds149268.mlab.com:49268/todo'
-// }
+let db = {
+	localhost: 'mongodb://127.0.0.1:27017/Todo',
+	mLab: 'mongodb://bharti25:Bh@rtiIsro2511@ds149268.mlab.com:49268/todo'
+}
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/Todo');
+mongoose.connect(db.localhost || db.mLab);
 
 module.exports = {
 	mongoose
