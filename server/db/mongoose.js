a445b87e045ce4a85ebebe404362a45e63a3ff7b
Bharti Bulchandani <bharti@paynpark.xyz>
2018-07-09 12:19:02 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
 	localhost: 'mongodb://127.0.0.1:27017/Todo',
-	mLab: 'mongodb://bharti25:Bh@rtiIsro2511@ds131551.mlab.com:31551/todoapp'
+	mLab: 'mongodb://bharti.bulchandani7@gmail.com:Bh@rtiIsro2511@ds131551.mlab.com:31551/todoapp'
 }
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Todo');
